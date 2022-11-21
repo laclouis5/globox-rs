1c5f7b272b03d9d23d297056bed17f26c969b5d2
Louis Lac <lac.louis5@gmail.com>
2022-11-21 14:22:04 +0100

    CLI

--- src/cli.rs
+++ src/cli.rs
@@ -1,16 +1,16 @@
 use crate::annotationset::AnnSet;
 
 use std::{
-    path::PathBuf,
+    path::{PathBuf},
     time::Instant,
 };
 
 use clap::{Parser, Args, Subcommand, ValueEnum};
 
 #[derive(Parser)]
-#[command(about = "A tool to parse, convert and evaluate bounding box annotations")]
+#[command(version, about)]
 struct Cli {
-    #[arg(short, long, help = "Make the operation more talkative")]
+    #[arg(short, long, help = "Make the operations more talkative")]
     verbose: bool,
 
     #[command(subcommand)]
@@ -19,31 +19,102 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Parse and display basic statistics.
+    /// Parse and display basic statistics
     Parse(Parse),
+
+    /// Convert bounding box annotations between formats
+    Convert(Convert),
+
+    /// Evaluate bounding box annotations and predictions
+    Eval(Eval),
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
-enum AnnFormat {
+enum SrcAnnFmt {
     Coco, Cvat, Imagenet, Labelme, OpenImage, Yolo, PascalVoc
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum DestAnnFmt {
+    Coco, Cvat, Imagenet, Labelme, OpenImage, Yolo, PascalVoc, Vit
 }
 
 #[derive(Args)]
 struct Parse {
     #[arg(help = "The format of the annotations")]
-    format: AnnFormat,
+    format: SrcAnnFmt,
 
     #[arg(help = "The file or directory path of the annotations")]
     path: PathBuf, 
 
-    #[arg(long, help = "The image directory for the annotations")]
+    #[arg(long, help = "The image directory of the annotations")]
     imgs_path: Option<PathBuf>,
 
     #[arg(long, default_value = "jpg", help = "The image extension (YOLO only)")]
     img_ext: String,
 
-    #[arg(long, help = "Whether the confidence is stored last or not (YOLO only)")]
+    #[arg(long, help = "Whether or not the confidence is stored in last position (YOLO only)")]
     conf_last: bool,
+}
+
+#[derive(Args)]
+struct Convert {
+    #[arg(help = "The format of the input annotations")]
+    src: SrcAnnFmt,
+
+    #[arg(help = "The file or directory path of the input annotations")]
+    src_path: PathBuf,
+
+    #[arg(help = "The format of the output annotations")]
+    dst: DestAnnFmt,
+
+    #[arg(help = "The file or directory path of the output annotations")]
+    dst_path: PathBuf,
+
+    #[arg(long, help = "The image directory of the input annotations")]
+    imgs_path: Option<PathBuf>,
+
+    #[arg(long, default_value = "jpg", help = "The image extension of input annotation images (YOLO only)")]
+    src_img_ext: String,
+
+    #[arg(long, help = "Whether or not the confidence of input annotations is stored in last position (YOLO only)")]
+    src_conf_last: bool,
+
+    #[arg(long, default_value = "jpg", help = "The image extension of output annotation images (YOLO only)")]
+    dst_img_ext: String,
+
+    #[arg(long, help = "Whether or not the confidence of output annotations should be stored in last position (YOLO only)")]
+    dst_conf_last: bool,
+}
+
+#[derive(Args)]
+struct Eval {
+    #[arg(help = "The format of the ground truth annotations")]
+    gts_fmt: SrcAnnFmt,
+
+    #[arg(help = "The file or directory path of the ground truth annotations")]
+    gts_path: PathBuf,
+
+    #[arg(help = "The format of the predicted annotations")]
+    dets_fmt: SrcAnnFmt,
+
+    #[arg(help = "The file or directory path of the predicted annotations")]
+    dets_path: PathBuf,
+
+    #[arg(long, help = "The image directory of the annotations")]
+    imgs_path: Option<PathBuf>,
+
+    #[arg(long, default_value = "jpg", help = "The image extension of ground truth annotation images (YOLO only)")]
+    gts_img_ext: String,
+
+    #[arg(long, help = "Whether or not the confidence of ground truth annotations is stored in last position (YOLO only)")]
+    gts_conf_last: bool,
+
+    #[arg(long, default_value = "jpg", help = "The image extension of predicted annotation images (YOLO only)")]
+    dets_img_ext: String,
+
+    #[arg(long, help = "Whether or not the confidence of predicted annotations is stored in last position (YOLO only)")]
+    dets_conf_last: bool,
 }
 
 pub fn run() {
@@ -62,17 +133,17 @@ pub fn run() {
             } = parse;
 
             let annset = match format {
-                AnnFormat::Coco => {
+                SrcAnnFmt::Coco => {
                     AnnSet::parse_coco(path.as_path())
                         .expect("failed to parse")
                 }, 
                 
-                AnnFormat::Cvat => {
+                SrcAnnFmt::Cvat => {
                     AnnSet::parse_cvat(path.as_path())
                         .expect("failed to parse")
                 },
 
-                AnnFormat::OpenImage => {
+                SrcAnnFmt::OpenImage => {
                     let imgs_path = imgs_path
                         .expect("parsing OpenImage requires `imgs_path`");
 
@@ -80,22 +151,22 @@ pub fn run() {
                         .expect("failed to parse")
                 },
 
-                AnnFormat::Labelme => {
+                SrcAnnFmt::Labelme => {
                     AnnSet::parse_labelme(path.as_path())
                         .expect("failed to parse")
                 },
 
-                AnnFormat::PascalVoc => {
+                SrcAnnFmt::PascalVoc => {
                     AnnSet::parse_pascal_voc(path.as_path())
                         .expect("failed to parse")
                 },
 
-                AnnFormat::Imagenet => {
+                SrcAnnFmt::Imagenet => {
                     AnnSet::parse_imagenet(path.as_path())
                         .expect("failed to parse")
                 }
 
-                AnnFormat::Yolo => {
+                SrcAnnFmt::Yolo => {
                     let imgs_path = imgs_path
                         .expect("parsing YOLO requires `imgs_path`");
 
@@ -110,6 +181,14 @@ pub fn run() {
             };
 
             println!("Parsed {} annotations in {:#?}.", annset.items.len(), time.elapsed());
+        },
+
+        Commands::Convert(_) => {
+            todo!("not yet implemented");
+        },
+
+        Commands::Eval(_) => {
+            todo!("not yet implemented");
         }
     }
 }
